825a8fa0f5b3d4fba0857de59c7c9043fdb88e63
e16din <bigusov@gmail.com>
2017-12-26 07:48:56 +0300

    Update example

--- app/src/main/java/com/e16din/topactivity/App.kt
+++ app/src/main/java/com/e16din/topactivity/App.kt
@@ -2,11 +2,16 @@ package com.e16din.topactivity
 
 
 import android.app.Application
+import android.util.Log
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        initTopActivity()
+
+        initTopActivity { activity ->
+            val message = if (activity != null) "activity: " + activity.javaClass.simpleName else "exit!"
+            Log.d("debug", message)
+        }
     }
 }
--- app/src/main/java/com/e16din/topactivity/SecondActivity.kt
+++ app/src/main/java/com/e16din/topactivity/SecondActivity.kt
@@ -4,13 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
 
-        startActivity(Intent(this, SecondActivity::class.java))
-        //todo: create example
+        startActivity(Intent(this, ThirdActivity::class.java))
     }
 }
